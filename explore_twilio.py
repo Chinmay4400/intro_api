4e92c22e7b33c25026b48c8ac5eab87fef12a328
Chinmay4400 <badjatyachinmay@gmail.com>
2020-05-31 21:12:05 +0530

    removed exposed keys

--- explore_twilio.py
+++ explore_twilio.py
@@ -1,21 +1,22 @@
 #Messing around with twilio
 
+import os
 from twilio.rest import Client
 
-client = Client("AC63386df5b8080869ee943a3b8e9cee74",
-                "b1b73a8e65f196f026f62cfde691ca7b"
+client = Client(os.environ['SID'],
+                os.environ['auth']
                 )
 
 for msg in client.messages.list():
     print(f"Deleting msg {msg.body}")
     msg.delete()
 
-# msg = client.messages.create(
-#         to = "+919826700551",
-#         from_ ="+12056712324",
-#         body = "Hello from python"
-#     )
+msg = client.messages.create(
+        to = "+919826700551",
+        from_ ="+12056712324",
+        body = "Hello from python"
+    )
 
-# print(f"created new msg: {msg.sid}")
+print(f"created new msg: {msg.sid}")
 
 
